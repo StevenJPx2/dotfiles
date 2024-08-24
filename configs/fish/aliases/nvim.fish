e209047f531b53fd74ab4b944851263f4cf78195
Steven John <stevenjpx2@gmail.com>
2024-08-24 18:26:17 +0530

    fix(nvim.fish): fix f command

--- configs/fish/aliases/nvim.fish
+++ configs/fish/aliases/nvim.fish
@@ -1,5 +1,3 @@
 alias v='nvim'
 alias vfl='v +tabnew +FlutterRun +only +tabn'
-alias f='v `fzf -i --ansi \
-  --preview="bat --color=always --style=numbers --line-range :500 {}"\
-  --preview-window=right:60%:wrap`'
+alias f='v $(fzf -i --ansi --preview="bat --color=always --style=numbers --line-range :500 {}" --preview-window=right:60%:wrap)'
