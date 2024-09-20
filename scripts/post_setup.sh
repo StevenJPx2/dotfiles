e6fd16d4c220031c57bbc1f8277770d18340edd1
Steven John <stevenjpx2@gmail.com>
2024-09-20 14:04:53 +0530

    fix: move one-time scripts to setup and post-setup scripts

--- scripts/post_setup.sh
+++ scripts/post_setup.sh
@@ -3,5 +3,14 @@
 # Init Rust
 rustup-init -y
 source "$HOME/.cargo/env.fish"
+
 # Fish
+if [ "$SHELL" != /opt/homebrew/bin/fish ]; then
+  chsh -s /opt/homebrew/bin/fish
+fi
+
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+# Yabai
+
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 "$(which yabai)" | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,13 @@
 
 export PATH="$PATH:/opt/homebrew/bin"
 
+# Fish settings
+if grep -q /opt/homebrew/bin/fish /etc/shells; then
+  echo "Fish is already installed"
+else
+  echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
+fi
+
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
