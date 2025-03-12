ce6da312d6e370c88af6a1bdb561531e87971ef4
Steven John <stevenjpx2@gmail.com>
2025-03-12 13:51:50 +0530

    feat: add smug config

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,10 +1,16 @@
+## Env vars
 set -gx EDITOR "/opt/homebrew/bin/nvim"
 set -gx NVM_DIR "$HOME/.nvm"
 set -gx GIT_EDITOR $EDITOR
+
 source "$HOME/.cargo/env.fish"
 set -q XDG_CONFIG_HOME || set XDG_CONFIG_HOME "$HOME/.config"
 
+## Path appends
+
 fish_add_path /opt/homebrew/bin
+fish_add_path ~/go/bin
+
 
 for file in $XDG_CONFIG_HOME/fish/aliases/*.fish
   source $file
