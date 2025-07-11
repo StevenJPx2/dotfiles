d83e21aec32c4e47266bd6ce2ab41dcf9dc9c84a
Steven John <stevenjpx2@gmail.com>
2025-07-11 14:15:35 +0400

    fix(neovim): exclude vue_ls from mason until vuels 3 support

--- configs/neovim/lua/plugins/lsp/mason.lua
+++ configs/neovim/lua/plugins/lsp/mason.lua
@@ -13,7 +13,7 @@ return {
 		---@module "mason-lspconfig"
 		---@type MasonLspconfigSettings
 		opts = {
-			automatic_enable = true,
+			automatic_enable = { exclude = { "vue_ls" } }, -- until fix for https://github.com/mason-org/mason-lspconfig.nvim/issues/587
 			ensure_installed = {
 				"vtsls",
 				"vue_ls",
